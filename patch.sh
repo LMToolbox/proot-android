8c19e9ec0ab235fc80e4cf68a3f1253abfd4c6b4
LukeMech <81306360+LukeMech@users.noreply.github.com>
2025-10-02 17:35:56 +0200

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -2,12 +2,19 @@
 
 # Patch binaries to make them execute from native library path
 
+. ./config
+
+for ARCH in $ARCHS
+do
+
+set-arch $ARCH
+
 mv $INSTALL_ROOT/bin/proot-userland $INSTALL_ROOT/libproot-bin.so
 mv $INSTALL_ROOT/libexec/proot/loader $INSTALL_ROOT/libproot-loader.so
 mv $INSTALL_ROOT/libexec/proot/loader32 $INSTALL_ROOT/libproot-loader32.so || true
 
 # Create wrapper script
-cat > "$DIST_DIR/libproot.so" <<EOF
+cat > "$INSTALL_ROOT/libproot.so" <<EOF
 #!/system/bin/sh
 dir="\$(cd "\$(dirname "\$0")"; pwd)"
 unset LD_PRELOAD
@@ -17,8 +24,10 @@ export PROOT_LOADER_32="\$dir/libproot-loader32.so"
 exec "\$dir/libproot-bin.so" "\$@"
 EOF
 
-chmod 755 "$DIST_DIR/libproot.so"
-dos2unix "$DIST_DIR/libproot.so"
+chmod 755 "$INSTALL_ROOT/libproot.so"
+dos2unix "$INSTALL_ROOT/libproot.so"
 
 rm -rf $INSTALL_ROOT/bin
 rm -rf $INSTALL_ROOT/libexec
+
+done
