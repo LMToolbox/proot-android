3fd5782a24579f6cebbcaf08c351e0a0afe6b5ca
LukeMech <lukaszjajkommz@gmail.com>
2025-10-02 21:27:26 +0200

    add termux core building

--- termux-exec-build.sh
+++ termux-exec-build.sh
@@ -5,6 +5,15 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 . "$SCRIPT_DIR/config"
 
+if [ -d "termux-core-package-$TERMUX_CORE_V" ] ; then exit 0 ; fi
+wget -O - https://github.com/termux/termux-core-package/archive/v${TERMUX_CORE_V}.tar.gz | tar -xzv
+
+cd "termux-core-package-$TERMUX_CORE_V"
+git clone https://github.com/termux/termux-packages.git
+export TERMUX_PKGS__BUILD__REPO_ROOT_DIR="$(realpath "./termux-packages")"
+make
+make install
+
 if [ -d "termux-exec-package-$TERMUX_EXEC_V" ] ; then exit 0 ; fi
 wget -O - https://github.com/termux/termux-exec-package/archive/v${TERMUX_EXEC_V}.tar.gz | tar -xzv
 
